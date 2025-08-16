5dec22bafc7af389317d65cd5ff25e56d2c45a6e
Noemí Mira <noemi.miranavarro@gmail.com>
2025-08-16 17:28:26 +0200

    done

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // IMPORT PACKAGES
+
 // Here you should import the required packages for your Express app: `express` and `morgan`
+const express = require('express');
+const morgan = require('morgan');
+const projects = require('./data/projects.json');
+const articles = require('./data/articles.json');
 
 
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
+const app = express();
 
 
 
@@ -13,13 +19,44 @@
 // - `express.static()` to serve static files from the `public` folder
 // - `express.json()` to parse incoming requests with JSON payloads
 // - `morgan` logger to log all incoming requests
+app.use(express.static('public'));
+app.use(express.json());
+app.use(morgan("dev"));
 
 
 
 // ROUTES
 // Start defining your routes here:
+app.get("/", (request, response) => {
+    console.log(request);
+    response.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/blog", (request, response) => {
+    console.log(request);
+    response.sendFile(__dirname + "/views/blog.html");
+});
+
+app.get("/api/projects", (request, response) => {
+    console.log(request);
+    response.json(projects);
+});
+
+app.get("/api/articles", (request, response) => {
+    console.log(request);
+    response.json(articles);
+});
+
+app.use((request, response) => {
+    console.log(request);
+    response.status(404).sendFile(__dirname + "/views/not-found.html");
+});
+
 
 
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+app.listen(5005, () => {
+    console.log("Server listening on port 5005");
+});
